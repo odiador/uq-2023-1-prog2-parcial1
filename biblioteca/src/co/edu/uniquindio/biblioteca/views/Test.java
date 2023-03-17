16e1f542c99c1cbe4f2f7366f7eccb58ed223088
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-03-16 22:05:06 -0500

    Update Test.java

--- biblioteca/src/co/edu/uniquindio/biblioteca/views/Test.java
+++ biblioteca/src/co/edu/uniquindio/biblioteca/views/Test.java
@@ -1,21 +1,17 @@
 package co.edu.uniquindio.biblioteca.views;
 
-import java.util.ArrayList;
-
 import co.edu.uniquindio.biblioteca.model.Biblioteca;
-import co.edu.uniquindio.biblioteca.model.Estudiante;
 
 public class Test {
 
 	public static void main(String[] args) {
 
 		String nombre = "Juan";
-		String apellidos = "arias muñoz";
+		String apellidos = "arias muï¿½oz";
 		String resultado = "";
-		
-		Biblioteca biblioteca = new Biblioteca("Mi biblioteca","calle 12","1234567");
-		
-		
+
+		Biblioteca biblioteca = new Biblioteca("Mi biblioteca", "calle 12", "1234567");
+
 		try {
 			resultado = biblioteca.crearEstudiante(nombre, apellidos, 20, "sistemas", "activo", "12345");
 		} catch (Exception e) {
@@ -45,10 +41,6 @@ public class Test {
 			resultado = e.getMessage();
 		}
 		System.out.println(resultado);
-		
-
-
-		
 
 	}
 
